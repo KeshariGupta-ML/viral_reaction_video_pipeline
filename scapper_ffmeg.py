5e299e1b2041cb97b8ceb7941e45bc240561fa32
shivagupta-aiml <shivagupta2706@gmail.com>
2026-09-03 10:14:59 +0200

    added high quality video scapper2

--- scapper_ffmeg.py
+++
@@ -1,8 +0,0 @@
-import shutil
-import yt_dlp
-
-print(f"FFmpeg detected by Python: {shutil.which('ffmpeg')}")
-
-url = "https://www.youtube.com/shorts/LtU_3Ep6qLE"
-with yt_dlp.YoutubeDL({'listformats': True}) as ydl:
-    ydl.extract_info(url, download=False)
